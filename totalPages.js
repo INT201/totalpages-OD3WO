ed6ffe46c8e53b8ff29d848c3dc2121de06aedc6
teerasak <teerasak.jiar@kmutt.ac.th>
2022-09-12 12:15:14 +0700

    Success

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,21 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  let getTotalPages;
+  let getTotalRows;
+  if (rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0) {
+    getTotalPages = 1;
+    return getTotalPages;
+  } else if (arrayItems === null || arrayItems === undefined) {
+    getTotalPages = undefined;
+    return getTotalPages;
+  } else {
+    for (let i of arrayItems) {
+      getTotalRows = i;
+    }
+    getTotalPages = getTotalRows/rowsPerPage;
+    return Math.ceil(getTotalPages);
+  }
 }
 module.exports = totalPages
+// console.log(testTotalPages([1, 2, 3, 4, 5, 6], 2))
